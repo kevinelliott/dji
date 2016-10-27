f429ba77066ab16427893ca1207bfd2db8e25ffa
Kevin Elliott <kevin@welikeinc.com>
2016-10-27 03:57:52 -0700

    Fix for the fedex command

--- lib/dji/commands/fedex/fedex_command.rb
+++ lib/dji/commands/fedex/fedex_command.rb
@@ -4,14 +4,14 @@ module DJI
       desc 'fedex COUNTRY_CODE POSTAL_CODE', 'track FedEx by reference'
       option :country_code, required: true, aliases: :c
       option :postal_code, required: true, aliases: :p
-      option :reference, aliases: :ref, default: 'DJIGOODS'
-      option :repeat, aliases: :r
+      option :reference, required: false, aliases: :ref, default: 'DJIGOODS'
+      option :repeat, required: false, aliases: :r
 
       def fedex
         if options[:repeat].present?
           interval = options[:repeat].to_i.presence || 300
           puts
-          puts "Requesting FedEx tracking by reference #{reference} every #{interval} seconds. Press CONTROL-C to stop..."
+          puts "Requesting FedEx tracking by reference #{options[:reference]} every #{interval} seconds. Press CONTROL-C to stop..."
 
           while true
             DJI::Fedex.track_by_reference(options[:country_code], options[:postal_code], options[:reference])
