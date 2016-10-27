414dfda73bbefa02ade6dce8bb05e155f5ef0430
Kevin Elliott <kevin@welikeinc.com>
2016-10-27 01:31:05 -0700

    Add in support for repeating the order tracking by an interval.

--- lib/dji/commands/track/track_command.rb
+++ lib/dji/commands/track/track_command.rb
@@ -4,9 +4,21 @@ module DJI
       desc 'track ORDER_NUMBER PHONE_TAIL', 'track an order'
       option :order_number, required: true, aliases: :o
       option :phone_tail, required: true, aliases: :p
+      option :repeat, aliases: :r
 
       def track
-        DJI::OrderTracking.tracking_details(options[:order_number], options[:phone_tail])
+        if options[:repeat].present?
+          interval = options[:repeat].to_i.presence || 300
+          puts
+          puts "Requesting order tracking details every #{interval} seconds. Press CONTROL-C to stop..."
+
+          while true
+            DJI::OrderTracking.tracking_details(options[:order_number], options[:phone_tail])
+            sleep(interval)
+          end
+        else
+          DJI::OrderTracking.tracking_details(options[:order_number], options[:phone_tail])
+        end
       end
     end
   end
