f72dd22f74ad6c05617ace47996ccde1e0fba6f6
Kevin Elliott <kevin@welikeinc.com>
2016-10-26 20:26:12 -0700

    Debugging

--- lib/dji.rb
+++ lib/dji.rb
@@ -1,5 +1,6 @@
 require 'active_support'
 require 'active_support/dependencies/autoload'
+require 'active_support/descendants_tracker'
 require 'active_support/core_ext/module/delegation'
 
 require 'dji/version'
@@ -7,6 +8,7 @@ require 'dji/version'
 module DJI
   extend ActiveSupport::Autoload
 
+  puts ActiveSupport::Dependencies.autoload_paths
   autoload :Command
   autoload :OrderTracking
 end
